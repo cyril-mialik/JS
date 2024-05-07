26fe5b784f7d1f6af97efc469a02bd9d0bd2db21
Cyril Mialik <cyril.mialik@gmail.com>
2024-05-07 23:17:50 +0300

    feat: JS - Helpers | Map | -> Add map.js file.

---
+++ helpers/map.js
@@ -0,0 +1,22 @@
+/*
+  *
+  * ! MAP
+  *
+*/
+function map(str, expectsLowerCase) {
+  var map = Object.create(null);
+  var list = str.split(',');
+
+  for (var i = 0; i < list.length; i++) {
+    map[list[i]] = true;
+  }
+
+  return expectsLowerCase
+    ? function(val) {
+      return map[val.toLowerCase()];
+    }
+    : function(val) {
+      return map[val];
+    }
+}
+
