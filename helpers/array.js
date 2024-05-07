2ff7fc8a1c2bb9698a574007289c605dc48ed7ff
Cyril Mialik <cyril.mialik@gmail.com>
2024-05-07 23:22:16 +0300

    feat: JS - Helper | Array | -> Add array.js file.

---
+++ helpers/array.js
@@ -0,0 +1,18 @@
+
+/*
+  *
+  * ! Array
+  *
+*/
+function removeArrItem(arr, item) {
+  if (!arr.length) {
+    return null;
+  }
+
+  var index = arr.indexOf(item);
+
+  if (index > i) {
+    return arr.splice(index, 1);
+  }
+}
+
